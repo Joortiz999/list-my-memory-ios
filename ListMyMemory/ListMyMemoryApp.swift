9473bc12fac855655812e5762cb238d1f8de3d67
Jorge Ortiz <jaortiz@bpd.com.do>
2023-03-25 08:49:33 -0400

    Initial Commit

---
+++ ListMyMemory/ListMyMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListMyMemoryApp.swift
+//  ListMyMemory
+//
+//  Created by JORGE ANDRES ORTIZ RODRIGUEZ on 25/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ListMyMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
